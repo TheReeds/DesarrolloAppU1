893210b90ba6e97c34e6123df178866f0d238da5
TheReeds <george159357852@gmail.com>
2024-04-23 21:32:33 -0500

    as

--- ms-user-jwt/src/main/java/com/example/msuserjwt/MsUserJwtApplication.java
+++ ms-user-jwt/src/main/java/com/example/msuserjwt/MsUserJwtApplication.java
@@ -1,11 +1,7 @@
 package com.example.msuserjwt;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
@@ -14,17 +10,6 @@ public class MsUserJwtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsUserJwtApplication.class, args);
-	}
-
-	@Bean
-	public OpenAPI custumOpenAPI(){
-		return new OpenAPI().info (new Info()
-				.title("OPEN API MICROSERVICIO ")
-				.version("0.0.1")
-				.description("servicio web cursos")
-				.termsOfService("http://swagger.io/terms")
-				.license (new License().name("Apache 2.0").url("http://springdoc.org"))
-		);
 	}
 
 }
