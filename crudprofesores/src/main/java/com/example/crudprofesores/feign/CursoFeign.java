468682c6457b82f444b715802a981f901e6cad16
TheReeds <george159357852@gmail.com>
2024-04-25 01:51:05 -0500

    a

--- crudprofesores/src/main/java/com/example/crudprofesores/feign/CursoFeign.java
+++ crudprofesores/src/main/java/com/example/crudprofesores/feign/CursoFeign.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-cursos-service", path = "/cursos")
+@FeignClient(name = "ms-curso-service", path = "/cursos")
 public interface CursoFeign {
     @GetMapping("{id}")
     public ResponseEntity<CursoDto> buscarPorId(@PathVariable(required = true) Integer id);
