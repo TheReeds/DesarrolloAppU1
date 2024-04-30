32e3cb433c1e37fc928d97f18906be38cdf3fd99
TheReeds <george159357852@gmail.com>
2024-04-30 18:50:25 -0500

    a

--- crudcursos/src/main/java/org/example/mscurso/repository/EstudianteRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.mscurso.repository;
-
-import org.example.mscurso.entity.Curso;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EstudianteRepository extends JpaRepository<Curso, Integer> {
-}
