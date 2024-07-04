7cc38986536664f02a4e831cc61de03ca44faa30
TheReeds <george159357852@gmail.com>
2024-07-04 03:17:41 -0500

    a

--- ms-matricula/src/main/java/org/example/msmatricula/service/impl/SesionServiceImpl.java
+++ ms-matricula/src/main/java/org/example/msmatricula/service/impl/SesionServiceImpl.java
@@ -5,27 +5,43 @@ import org.example.msmatricula.Dto.TareaDto;
 import org.example.msmatricula.entity.Aula;
 import org.example.msmatricula.entity.Sesion;
 import org.example.msmatricula.entity.Tarea;
+import org.example.msmatricula.repository.AulaRepository;
 import org.example.msmatricula.repository.SesionRepository;
 import org.example.msmatricula.service.SesionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class SesionServiceImpl implements SesionService {
     @Autowired
     private SesionRepository sesionRepository;
+    @Autowired
+    private AulaRepository aulaRepository;
+
 
     @Override
     public SesionDto createSesion(SesionDto sesionDto) {
+        // Verificar si el aulaId existe en la base de datos
+        Optional<Aula> aulaOptional = aulaRepository.findById(sesionDto.getAulaId());
+        if (!aulaOptional.isPresent()) {
+            throw new RuntimeException("Aula con id " + sesionDto.getAulaId() + " no existe.");
+        }
+
+        // Crear una nueva entidad Sesion
         Sesion sesion = new Sesion();
         sesion.setNombre(sesionDto.getNombre());
         sesion.setTitulo(sesionDto.getTitulo());
         sesion.setDescripcion(sesionDto.getDescripcion());
-        sesion.setAula(new Aula(sesionDto.getAulaId()));
+        sesion.setAula(aulaOptional.get());
+
+        // Guardar la entidad Sesion
         Sesion savedSesion = sesionRepository.save(sesion);
+
+        // Convertir la entidad guardada a DTO y devolverla
         return convertToDto(savedSesion);
     }
 
